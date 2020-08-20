d20e51f515884bac87877a3ce3a645729e8c6e97
marti <marticraft680@gmail.com>
2020-08-20 13:24:50 -0400

    ?

--- main.py
+++ main.py
@@ -7,12 +7,33 @@ def read_token(file):
         lines = f.readlines()
         return lines[0].strip()
 
-client = commands.Bot(command_prefix = '!')
+prefix = '!'
+
+print(read_token('token.txt'))
+
+client = commands.Bot(command_prefix = prefix)
 
 bot_ping = '<@!725225223346978816>'
 
 @client.event
 async def on_ready():
     print('logged in as {0.user}'.format(client))
+
+@client.event
+async def on_member_join(member):
+    print('{} has joined one of the servers'.format(member))
+
+@client.event
+async def on_member_remove(member):
+    print('{} member has left a server'.format(member))
+    #delete the member's pokemon data
+
+
+# --- Commands ---
+@client.command(aliases=[bot_ping])
+async def ping(ctx):
+    await ctx.send('Hello there, (my prefix is ' + prefix + '). Do ' + prefix + 'help to see what you can do')
+# --- ---
+
 
 client.run(read_token('token.txt'))
