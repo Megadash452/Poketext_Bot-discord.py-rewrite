b98ae43893da1fa35839e304e39414ff941fbf4e
Megadash452 old laptop <marticraft680@gmail.com>
2020-08-20 03:29:26 -0400

    token.txt

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import discord
 from discord.ext import commands
 
+def read_token(file):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+        return lines[0].strip()
+
+token = read_token('token.txt')
+
 client = commands.Bot(command_prefix = '!')
 
+bot_ping = '<@!725225223346978816>'
+
+@client.event
+async def on_ready():
+    print('logged in as {0.user}'.format(client))
+
+client.run()
