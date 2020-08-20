54ff2f38b111ec04f4a8d9f6dfa1a795f6d69c96
Megadash452 old laptop <marticraft680@gmail.com>
2020-08-20 10:57:44 -0400

    files

---
+++ Pteam.py
@@ -0,0 +1,10 @@
+import Pokemon_dictionaries
+
+
+def post():
+    print('PokeText team game: Function post() found and executed')
+
+
+def start():
+    print('team game will start')
+    # --- Write game code HERE ---
