dc686b7115391bb3d90ba0750f742d41eb846b81
Megadash452 old laptop <marticraft680@gmail.com>
2020-08-22 04:19:37 -0400

    random?

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-import discord
+import discord, json, random
 from discord.ext import commands
 from discord.utils import get
 
@@ -60,7 +59,7 @@ async def on_member_remove(member):
 # --- Commands ---
 @client.command(aliases=[bot_ping])
 async def help(ctx):
-    prefix = client.command_prefix(ctx.guild, ctx.message)
+    prefix = client.command_prefix(ctx.guild, ctx.message)[0]
 
     embed = discord.Embed(title='Help', description='Ping me or use my prefix (` {} `) to use a Command\n\n\n'.format(prefix), colour=int(type_colors['PokeText']['hex'], 16))
 
@@ -81,11 +80,8 @@ async def help(ctx):
     )
     embed.add_field(
         name='Game Management Commands:',
-        value='\n\n\t-  ****:' +
-            '\n\n\t-  **y**:' +
-            '\n\n\t-  **y**:' +
-            '\n\n\t-  **y**:' +
-            '\n\n\t-  **y**:'
+        value='\n\n\t-  **ban**:' +
+            '\n\n\t-  **voteban**:'
     )
 
     await ctx.send(embed=embed)
@@ -126,14 +122,40 @@ async def invite(ctx):
     await ctx.send('https://discord.com/api/oauth2/authorize?client_id=725225223346978816&permissions=37584704&scope=bot')
 
 
+# --- Game Commands ---
+
+
+@client.command(aliases=['random'])
+async def rand(ctx):
+    random_num = random.randrange(1, 151, 1)
+
+    embed = discord.Embed(title=Pokemon[random_num].name, colour=int(type_colors[Pokemon[random_num].types[0]]['hex'], 16))
+
+    embed.add_field(name=Pokemon[random_num].poke_specie, value=Pokemon[random_num].desc)
+
+    if not Pokemon[random_num].types[1]:
+        embed.add_field(name='Type', value=Pokemon[random_num].types[0], inline=False)
+    else:
+        embed.add_field(name='Types', value=f'{Pokemon[random_num].types[0]}\n{Pokemon[random_num].types[1]}', inline=False)
+
+    embed.set_image(url=Pokemon[random_num].sprite['big']['url'])
+    #embed.set_thumbnail(url=Pokemon[random_num].sprite['small']['url'])
+    
+
+    await ctx.send(embed=embed)
+
+
 @client.command()
 async def info(ctx, *, mon):
-    try:
-        int(mon) + 1
-        call = int(mon)
-    except:
-        from Pokemon_dictionaries import Number_dictionary as Dic
-        call = int(Dic[mon.lower()])
+    if not mon == 'random':
+        try:
+            int(mon) + 1
+            call = int(mon)
+        except:
+            from Pokemon_dictionaries import Number_dictionary as Dic
+            call = int(Dic[mon.lower()])
+    else:
+        call = random.randrange(1, 151+1, 1)
 
     embed = discord.Embed(title=Pokemon[call].name, colour=int(type_colors[Pokemon[call].types[0]]['hex'], 16))
 
