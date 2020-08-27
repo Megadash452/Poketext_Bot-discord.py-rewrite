b2e7cb29e456a116a66f21f40819bbd8739298ec
marti <marticraft680@gmail.com>
2020-08-26 21:32:08 -0400

    added item classes

--- Mon_data/items.py
+++ Mon_data/items.py
@@ -1,4 +1,39 @@
-class Items:
+class Item:
     def __init__(self, name, type):
         self.name = name
         self.type = type
+
+
+class Pokeball(Item):
+    def __init__(self, name, type):
+        super.__init__(name, type)
+
+
+class Medicine(Item):
+    def __init__(self, name, type):
+        super.__init__(name, type)
+
+
+class Berry(Item):
+    def __init__(self, name, type):
+        super.__init__(name, type)
+
+
+class Battle_item(Item):
+    def __init__(self, name, type):
+        super.__init__(name, type)
+
+
+class Hold_item(Item):
+    def __init__(self, name, type):
+        super.__init__(name, type)
+
+
+class General_item(Item):
+    def __init__(self, name, type):
+        super.__init__(name, type)
+
+
+class Machine(Item):
+    def __init__(self, name, type):
+        super.__init__(name, type)
